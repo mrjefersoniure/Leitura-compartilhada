2b668838d320797a145a645921c10fb336fb18ee
JefersonIure <jefersoniurematos@gmail.com>
2025-12-02 18:11:21 -0300

    tratamento de erro no login

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Inicializa o DB e as tabelas
-require('./config/db'); 
+require('./config/db');
+
+// Middlewares
+app.use(cors()); // Permite acesso externo
+app.use(express.json());
+app.use(express.static('public')); 
 
 // Importa os roteadores
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 
-// Middlewares
-app.use(express.json());
-app.use(express.static('public')); 
-
-// Conexão das Rotas
+// Conecta as rotas
 app.use('/api/auth', authRoutes);
-app.use('/api/livros', bookRoutes); 
+app.use('/api/livros', bookRoutes);
 
-// Inicia o Servidor
+// Rota de teste
+app.get('/', (req, res) => {
+    res.send("API do projeto Leitura Compartilhada está funcionando 🚀");
+});
+
+// Inicia o servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
